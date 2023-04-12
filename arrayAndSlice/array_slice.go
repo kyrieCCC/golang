a4b6e1219705b53e6c37e1b958351280416b94d8
kyrieCCC <1186624948@qq.com>
2023-04-12 20:32:57 +0800

    feat: go-appendAndCopy

--- arrayAndSlice/array_slice.go
+++ arrayAndSlice/array_slice.go
@@ -13,6 +13,11 @@ func main(){
 	var slice = make([]int, 3)
 	slice2 := [] int {1,2,3,4,5}
 
+	//测试添加与拷贝
+	sliceAppend := []int{1,2,3}
+	sliceCopy := make([]int, 4)
+	sliceCopy = append(sliceCopy, 28)
+
 	fmt.Println(a)
 	fmt.Println(arr)
 	fmt.Println(slice)
@@ -20,6 +25,7 @@ func main(){
 	
 	slice_part(slice2)
 	slice_len_cap(slice)
+	slice_append_copy(sliceAppend, sliceCopy)
 }
 
 //切片截取
@@ -31,4 +37,13 @@ func slice_part(slice2 []int){
 //切片的len与cap方法
 func slice_len_cap(slice []int) {
 	fmt.Println(len(slice), cap(slice), slice)
+}
+
+//切片的append和copy方法
+func slice_append_copy(slice1, slice2 []int) {
+	slice1 = append(slice1, 43)
+	//这里的slice2我们传一个非空切片便于展示区别
+	copy(slice2, slice1)
+	fmt.Println(slice1)
+	fmt.Println(slice2)
 }
