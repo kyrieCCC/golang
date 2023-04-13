88826fbfa134bcbb25d2680522d5da807e6dd23b
kyrieCCC <1186624948@qq.com>
2023-04-13 17:24:11 +0800

    feat: go-hand

---
+++ hand/hand.go
@@ -0,0 +1,20 @@
+package main
+import "fmt"
+
+func main(){
+	//定义一个变量和一个指针
+	a := 20
+	var ip *int
+
+	//将这个指针指向我们的a变量
+	ip = &a
+
+	//输出a的存储地址
+	fmt.Println("a变量的地址是", &a)
+
+	//输出ip指向的地址，这里因为是指向了a，所以是ip返回的是a的存储地址
+	fmt.Println("ip指针指向的地址是", ip)
+
+	//返回ip指向的地址对应的值
+	fmt.Println("ip指针指向的地址的值是", *ip)
+}
