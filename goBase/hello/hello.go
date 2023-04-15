71a69b707c6c2f69806c1021e4826b278381a312
kyrieCCC <1186624948@qq.com>
2023-04-15 23:26:28 +0800

    feat: go-Join-helloworld

--- goBase/hello/hello.go
+++ goBase/hello/hello.go
@@ -1,14 +1,25 @@
-//定义了一个包名，必须在源文件的第一行非注释来说明文件属于哪个包
-//每一个go程序都需要包含一个名为main的包
+// 定义了一个包名，必须在源文件的第一行非注释来说明文件属于哪个包
+// 每一个go程序都需要包含一个名为main的包
 package main
 
 // import "fmt" 告诉 Go 编译器这个程序需要使用 fmt 包（的函数，或其他元素）
 // fmt 包实现了格式化 IO（输入/输出）的函数。
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main(){
 	//换行
 	fmt.Println("hello world")
 
 	fmt.Print("hello world")
+
+	slices := []string {"hello", "world"}
+
+	outHello((slices))
+}
+
+func outHello(slice []string) {
+	fmt.Println(strings.Join(slice, "-"))
 }
