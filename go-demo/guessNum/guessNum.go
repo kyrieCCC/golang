109ac4c0070804056882f66b0679d716a7660720
kyrieCCC <1186624948@qq.com>
2023-04-17 22:53:57 +0800

    feat: go-demo-guess-3

--- go-demo/guessNum/guessNum.go
+++ go-demo/guessNum/guessNum.go
@@ -9,17 +9,24 @@ import (
 	"strings"
 )
 
+
+//实现一个简单的猜字游戏
 func main() {
 	maxNum := 100
 
+	//生成一个随机数，使用rand.Intn方法
 	randomNum := rand.Intn(maxNum)
 
 	// fmt.Println(randomNum)
 
 	fmt.Println("please input your number:")
 
+	//进行循环猜谜
 	for {
+		//创建一个新的可读流，并读取命令台输入的内容
 		reader := bufio.NewReader(os.Stdin)
+
+		//获取到输入的内容
 		input, err := reader.ReadString('\n')
 		if err != nil {
 			fmt.Println("error1", err)
@@ -27,8 +34,10 @@ func main() {
 			continue
 		}
 
+		//去除换行符
 		input = strings.TrimSuffix(input, "\r\n")
 
+		//将输入的内容转换为int整型
 		guess, err := strconv.Atoi(input)
 		if err != nil {
 			fmt.Println("error2", err)
@@ -37,6 +46,8 @@ func main() {
 		}
 		// fmt.Println("your guess number is:", guess)
 
+
+		//不断循环判断输入的数字大小，直到输入的结果正确
 		if guess < randomNum {
 			fmt.Println("a little small")
 		} else if guess > randomNum {
