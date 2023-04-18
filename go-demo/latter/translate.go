8b10234e722bc4b93646bf4e6c65cb11dfef3abd
kyrieCCC <1186624948@qq.com>
2023-04-18 21:57:27 +0800

    feat: go翻译小插件

--- go-demo/latter/translate.go
+++ go-demo/latter/translate.go
@@ -1,16 +1,57 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"os"
+	"bufio"
 	"strings"
 )
 
-func main() {
+type DictRequest struct {
+	TransType string `json:"trans_type"`
+	Source string `json:"source"`
+	UserId string `json:"user_id"`
+}
+
+
+type DictResponse struct {
+	Rc int `json:"rc"`
+	Wiki struct {
+	} `json:"wiki"`
+	Dictionary struct {
+		Prons struct {
+			EnUs string `json:"en-us"`
+			En string `json:"en"`
+		} `json:"prons"`
+		Explanations []string `json:"explanations"`
+		Synonym []string `json:"synonym"`
+		Antonym []string `json:"antonym"`
+		WqxExample [][]string `json:"wqx_example"`
+		Entry string `json:"entry"`
+		Type string `json:"type"`
+		Related []interface{} `json:"related"`
+		Source string `json:"source"`
+	} `json:"dictionary"`
+}
+
+
+func query(word string) {
 	client := &http.Client{}
-	var data = strings.NewReader(`{"trans_type":"en2zh","source":"good"}`)
+	// var data = strings.NewReader(`{"trans_type":"en2zh","source":"good"}`)
+	request := DictRequest{
+		TransType: "en2zh",
+		Source: word,
+	}
+	buf, err := json.Marshal(request)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var data = bytes.NewReader(buf)
 	req, err := http.NewRequest("POST", "https://lingocloud.caiyunapp.com/v1/dict", data)
 	if err != nil {
 		log.Fatal(err)
@@ -43,5 +84,55 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("%s\n", bodyText)
+	// fmt.Printf("%s\n", bodyText)
+
+	if resp.StatusCode != 200{
+		log.Fatal("bad request", resp.StatusCode)
+	}
+
+	var dictResponse DictResponse
+	//反序列化接收到的结果，并将结果保存在dictResponse当中
+	err = json.Unmarshal(bodyText, &dictResponse)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// fmt.Printf("%#v\n", dictResponse)
+	fmt.Println(word, "的翻译结果为：")
+	fmt.Println("音标：" ,"UK:", dictResponse.Dictionary.Prons.En, "US:", dictResponse.Dictionary.Prons.EnUs)
+
+	for _, item := range dictResponse.Dictionary.Explanations {
+		fmt.Println(item)
+	}
+}
+
+
+func main() { 
+	// if len(os.Args) != 2 {
+	// 	fmt.Fprintf(os.Stderr, `usage: simpleDict WORD example: simpledict hello`)
+	// 	os.Exit(1)
+	// }
+	// word := os.Args[1]
+	// query(word)
+
+	fmt.Println("please input a latter: ")
+	reader := bufio.NewReader(os.Stdin)
+
+		//获取到输入的内容
+		word, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("error1", err)
+			return
+		}
+
+		//去除换行符
+		word = strings.TrimSuffix(word, "\r\n")
+
+		//将输入的内容转换为int整型
+		// guess, err := strconv.Atoi(input)
+		if err != nil {
+			fmt.Println("error2", err)
+			return
+		}
+		// fmt.Println(input)
+		query(word)
 }
