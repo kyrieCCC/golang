6e5e87336c2ea0ffabbfcbdc00e225e4de3c0a8b
kyrieCCC <1186624948@qq.com>
2023-04-20 22:22:41 +0800

    feat: go-routine-2

--- goBase/goroutines/goroutine.go
+++ goBase/goroutines/goroutine.go
@@ -6,11 +6,13 @@ import (
 )
 
 func main() {
-
+	//一个for循环，每次执行的时候都使用go关键字创建一个协程
 	for i := 0; i < 5; i++ {
+		// 创建一个协程
 		go printNum(i)
 	}
 
+	//阻塞时间，保证在协程内容执行完前主线程不退出
 	time.Sleep(time.Second)
 }
 
