de1fd7e0f4c84a67b978a3ed7eae3d3eae142c22
kyrieCCC <1186624948@qq.com>
2023-04-23 23:15:30 +0800

    feat: go-leetcode-231

--- go-leetcode/231.isPowerOfTwo/index.go
+++ go-leetcode/231.isPowerOfTwo/index.go
@@ -3,9 +3,16 @@ package main
 func main(){
 	res1 := isPowerOfTwo1(5)
 
+	res2 := isPowerOfTwo3(123)
+
+	res3 := isPowerOfTwo2(256)
+	
 	println(res1)
+	println(res2)
+	println(res3)
 }
 
+//1. 循环
 func isPowerOfTwo1(n int) bool {
 	if n == 1 {
 		return true
@@ -19,3 +26,20 @@ func isPowerOfTwo1(n int) bool {
 	}
 	return false
 }
+
+//2. 递归
+func isPowerOfTwo2(n int) bool {
+	if n == 1 {
+		return true
+	}
+	if n > 2 && n != 1 {
+		return false
+	}
+	return isPowerOfTwo2(n / 2)
+}
+
+//3. 位运算
+func isPowerOfTwo3(n int) bool {
+	return n > 0 && n & (n - 1) == 0
+}
+// 击败100% ！！！！
