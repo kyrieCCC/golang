06b0a505556146aac8e3dbd3decae6cfaf5591e5
kyrieCCC <1186624948@qq.com>
2023-04-25 11:16:41 +0800

    feat: go-leetcode-2682

--- go-leetcode/268.missingNumber/idnex.go
+++ go-leetcode/268.missingNumber/idnex.go
@@ -10,8 +10,14 @@ func main() {
 	println(missingNumber(nums1))
 	println(missingNumber(nums2))
 	println(missingNumber(nums3))
+
+	println("--------------------------------")
+	println(missingNumber_map(nums1))
+	println(missingNumber_map(nums2))
+	println(missingNumber_map(nums3))
 }
 
+//解法一 暴力循环
 func missingNumber(nums []int) int {
 	sort.Ints(nums)
 
@@ -21,4 +27,18 @@ func missingNumber(nums []int) int {
 		}
 	}
 	return len(nums)
+}
+
+//解法二 哈希
+func missingNumber_map(nums []int) int {
+	myMap := map[int]bool{}
+
+	for _, item := range nums {
+		myMap[item] = true
+	}
+	for i := 0; ;i++ {
+		if !myMap[i] {
+			return i
+		}
+	}
 }
