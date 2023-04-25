54ed645139b9e4bfad628d0933b2ca2407e9404d
kyrieCCC <1186624948@qq.com>
2023-04-25 11:31:25 +0800

    feat: go-leetcode-268-done

--- go-leetcode/268.missingNumber/idnex.go
+++ go-leetcode/268.missingNumber/idnex.go
@@ -15,6 +15,11 @@ func main() {
 	println(missingNumber_map(nums1))
 	println(missingNumber_map(nums2))
 	println(missingNumber_map(nums3))
+
+	println("--------------------------------")
+	println(missingNumber_math(nums1))
+	println(missingNumber_math(nums2))
+	println(missingNumber_math(nums3))
 }
 
 //解法一 暴力循环
@@ -41,4 +46,19 @@ func missingNumber_map(nums []int) int {
 			return i
 		}
 	}
+}
+
+//解法三 数学思想
+func missingNumber_math(nums []int) int {
+	totalSum := 0
+	for i := 0; i <= len(nums); i++ {
+		totalSum += i
+	}
+
+	numsSum := 0
+	for _, item := range nums{
+		numsSum += item
+	}
+
+	return totalSum - numsSum
 }
