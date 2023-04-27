78a3306b5b62dbd572072cfbb95a46f46608db9d
kyrieCCC <1186624948@qq.com>
2023-04-27 16:49:30 +0800

    go-leetcode-342

--- go-leetcode/342.isPowerOfFour/index.go
+++ go-leetcode/342.isPowerOfFour/index.go
@@ -9,8 +9,13 @@ func main() {
 	println(isPowerOfFour(a1))
 	println(isPowerOfFour(a2))
 	println(isPowerOfFour(a3))
+	println("=====================================")
+	println(isPowerOfFour_deep(a1))
+	println(isPowerOfFour_deep(a2))
+	println(isPowerOfFour_deep(a3))
 }
 
+//解法一 循环
 func isPowerOfFour(n int) bool {
 	if n == 1 {
 		return true
@@ -22,4 +27,9 @@ func isPowerOfFour(n int) bool {
 		i = i * 4
 	}
 	return false
+}
+
+//解法二 递归
+func isPowerOfFour_deep(n int) bool {
+	return n > 0 && n & (n - 1) == 0 && n % 3 == 1
 }
