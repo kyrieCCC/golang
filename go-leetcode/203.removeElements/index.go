bcede770a6451662d948b3fd419b3228a08595ad
kyrieCCC <1186624948@qq.com>
2023-04-30 22:10:39 +0800

    feat: go-leetcode-203-2

--- go-leetcode/203.removeElements/index.go
+++ go-leetcode/203.removeElements/index.go
@@ -6,9 +6,20 @@ type ListNode struct {
  }
 
 func main(){
-
+	var ln ListNode
+	ln.Val = 1
+	ln.Next = &ln
+	removeElements(&ln, 1)
 }
 
 func removeElements(head *ListNode, val int) *ListNode {
-	
+	newNode := &ListNode{Next: head}
+	for node := newNode; node.Next != nil ;{
+		if node.Next.Val == val {
+			node.Next = node.Next.Next
+		} else {
+			node = node.Next
+		}
+	}
+	return newNode.Next
 }	
