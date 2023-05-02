05f201a1961d10b8e5f6f19bb9c7804a1c7f84b4
kyrieCCC <1186624948@qq.com>
2023-05-02 11:03:08 +0800

    feat: go-leetcode-217-2

--- go-leetcode/217.containsDuplicate/index.go
+++ go-leetcode/217.containsDuplicate/index.go
@@ -1,13 +1,18 @@
 package main
 
+import "sort"
+
 func main() {
-	nums1 := []int{1,2,3,1}
-	nums2 := []int{1,2,3,4}
+	nums1 := []int{1, 2, 3, 1}
+	nums2 := []int{1, 2, 3, 4}
 
 	println(containsDuplicate(nums1))
 	println(containsDuplicate(nums2))
-}
 
+	println("-------------------------------------------")
+	println(containsDuplicate_sort(nums1))
+	println(containsDuplicate_sort(nums2))
+}
 
 // 217. 存在重复元素
 // 给你一个整数数组 nums 。如果任一值在数组中出现 至少两次 ，返回 true ；
@@ -15,15 +20,28 @@ func main() {
 // 输入：nums = [1,2,3,1]
 // 输出：true
 
+// 解法一 哈希表
 func containsDuplicate(nums []int) bool {
 	myMap := make(map[int]bool, 0)
 
 	for _, item := range nums {
-		if(myMap[item]){
+		if myMap[item] {
 			return true
 		} else {
 			myMap[item] = true
 		}
 	}
+	return false
+}
+
+// 解法二 排序
+func containsDuplicate_sort(nums []int) bool {
+	sort.Ints(nums)
+
+	for i := 0; i < len(nums) - 1; i++ {
+		if nums[i] == nums[i + 1] {
+			return true
+		}
+	} 
 	return false
 }
