5992bb89cf16d94ee6463dd61b9a826e64190a1d
kyrieCCC <1186624948@qq.com>
2023-05-04 13:06:56 +0900

    feat: go-leetcode-125-2

--- go-leetcode/125.isPalinderome.js/index.go
+++ go-leetcode/125.isPalinderome.js/index.go
@@ -43,6 +43,10 @@ func isPalindrome(s string) bool {
     }
     return true
 }
+//首先先将字符串中的全部内容转换为小写形式
+//随后使用正则表达式来判断是否为字母或者是数字
+//将过滤后的结果拿出来
+//最后使用两个指针，一个在前一个在后来滑动判断是否为回文串
 
 func isalnum(ch byte) bool {
     return (ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z') || (ch >= '0' && ch <= '9')
